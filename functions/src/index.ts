77cfc85f522ccc356826223b03aead4899db6f77
Mikhayl Joshua Castro Abellanosa <luckeyepe@gmail.com>
2019-03-08 19:29:07 +0800

    finished the onUpdate

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -60,55 +60,91 @@ export const updateClartDetailsOnCartUpdate = functions.firestore
             console.log("Nothing changed");
             return null
         }else {
-            // @ts-ignore
-            const brokenPath: string[] = itemAfter.ref.path.split("/");
-            const invokerUid: string = brokenPath[1];
-            // @ts-ignore
-            const cartData = itemAfter.data();
-            // @ts-ignore
-            const cartCost = cartData.cart_item_price * cartData.cart_item_count;
+            if (change.after !== undefined && change.before !== undefined) {
+                const brokenPath: string[] = change.after.ref.path.split("/");
+                const invokerUid: string = brokenPath[1];
 
-            const cartDetailsPromise = await admin.firestore()
-                .doc("Cart_Details/"+invokerUid)
-                .get();
-
-            const cartDetails = cartDetailsPromise.data();
-            // @ts-ignore
-            let currentItemCount: number = cartDetails.cart_detail_item_count;
-            // @ts-ignore
-            let currentCost:number = cartDetails.cart_detail_cost;
-
-            // @ts-ignore
-            if (itemAfter.cart_item_count<itemBefore.cart_item_count){
+                const cartData = itemAfter;
                 // @ts-ignore
-                const countAfter = itemAfter.cart_item_count;
-                // @ts-ignore
-                const countBefore = itemBefore.cart_item_count;
-                currentItemCount = currentItemCount - countBefore + countAfter - 1;
+                const cartCost = cartData.cart_item_price * cartData.cart_item_count;
 
-                // @ts-ignore
-                const cartCostBefore = itemBefore.cart_item_price * itemBefore.cart_item_count;
-                currentCost = currentCost + cartCost - cartCostBefore;
-
-                return admin.firestore()
+                const cartDetailsPromise = await admin.firestore()
                     .doc("Cart_Details/" + invokerUid)
-                    .update({
-                        cart_detail_cost: currentCost,
-                        cart_detail_item_count: currentItemCount
-                    })
+                    .get();
+
+                const cartDetails = cartDetailsPromise.data();
+                // @ts-ignore
+                let currentItemCount: number = cartDetails.cart_detail_item_count;
+                // @ts-ignore
+                let currentCost: number = cartDetails.cart_detail_cost;
+
+                // @ts-ignore
+                if (itemAfter.cart_item_count < itemBefore.cart_item_count) {
+                    // @ts-ignore
+                    const countAfter = itemAfter.cart_item_count;
+                    // @ts-ignore
+                    const countBefore = itemBefore.cart_item_count;
+                    currentItemCount = currentItemCount - countBefore + countAfter;
+
+                    // @ts-ignore
+                    const cartCostBefore = itemBefore.cart_item_price * itemBefore.cart_item_count;
+                    currentCost = currentCost + cartCost - cartCostBefore;
+
+                    return admin.firestore()
+                        .doc("Cart_Details/" + invokerUid)
+                        .update({
+                            cart_detail_cost: currentCost,
+                            cart_detail_item_count: currentItemCount
+                        })
+                } else {
+                    // @ts-ignore
+                    const count = itemAfter.cart_item_count;
+                    // @ts-ignore
+                    const countBefore = itemBefore.cart_item_count;
+                    currentItemCount = currentItemCount - countBefore + count;
+                    // @ts-ignore
+                    const cartCostBefore = itemBefore.cart_item_price * itemBefore.cart_item_count;
+
+                    return admin.firestore()
+                        .doc("Cart_Details/" + invokerUid)
+                        .update({
+                            cart_detail_cost: currentCost + cartCost - cartCostBefore,
+                            cart_detail_item_count: currentItemCount
+                        })
+                }
+
             }else {
-                // @ts-ignore
-                const count = itemAfter.cart_item_count;
-                // @ts-ignore
-                const cartCostBefore = itemBefore.cart_item_price * itemBefore.cart_item_count;
-
-                return admin.firestore()
-                    .doc("Cart_Details/" + invokerUid)
-                    .update({
-                        cart_detail_cost: currentCost + cartCost - cartCostBefore,
-                        cart_detail_item_count: currentItemCount - 1 + count
-                    })
+                return null
             }
-
         }
+    });
+
+export const updateClartDetailsOnCartDelete = functions.firestore
+    .document("Cart/{userUid}/cart/{cartUid}")
+    .onDelete(async (snapshot, context) => {
+        const brokenPath: string[] = snapshot.ref.path.split("/");
+        const invokerUid: string = brokenPath[1];
+        const cartData = snapshot.data();
+        // @ts-ignore
+        const cartCost = cartData.cart_item_price * cartData.cart_item_count;
+
+        const cartDetailsPromise = await admin.firestore()
+            .doc("Cart_Details/"+invokerUid)
+            .get();
+
+        const cartDetails = cartDetailsPromise.data();
+        // @ts-ignore
+        const currentItemCount: number = cartDetails.cart_detail_item_count;
+        // @ts-ignore
+        const currentCost:number = cartDetails.cart_detail_cost;
+
+        // @ts-ignore
+        const cartCount = cartData.cart_item_count;
+
+        return admin.firestore()
+            .doc("Cart_Details/"+invokerUid)
+            .update({
+                cart_detail_cost: currentCost-cartCost,
+                cart_detail_item_count: currentItemCount- cartCount
+            })
     });
